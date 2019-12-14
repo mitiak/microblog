ba51ec753a2c1da83e10925b763b19281b3ddb5a
Dima Kovalchuk <dima@orca.security>
2019-12-14 22:53:25 +0200

    init the database and migrations

--- config.py
+++ config.py
@@ -1,5 +1,13 @@
 import os
 
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY', 'you-will-never-guess')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL', 'sqlite:///' + os.path.join(basedir, 'app.db')
+    )
+
