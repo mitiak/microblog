80602d43e15ba465211ce358fdd49bcf5f04a910
Dima Kovalchuk <dima@orca.security>
2019-12-17 12:40:57 +0200

    do not pass user

--- app/routes.py
+++ app/routes.py
@@ -11,7 +11,6 @@ from app.forms import LoginForm
 @app.route('/index')
 @login_required
 def index():
-    user = {'username': 'mitiak'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -23,7 +22,7 @@ def index():
         }
     ]
 
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home Page', posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
