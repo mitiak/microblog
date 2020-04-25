fb4c66ecee0e09353f39f845d33d17c25e139a02
Dima Kovalchuk <dima@orca.security>
2020-04-26 00:28:42 +0300

    add falsk-mail

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from logging.handlers import SMTPHandler, RotatingFileHandler
 
 from flask import Flask
 from flask_login import LoginManager
+from flask_mail import Mail
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
@@ -14,7 +15,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
-
+mail = Mail(app)
 
 if not app.debug:
     # email handler
